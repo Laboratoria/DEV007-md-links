8c4c5e6a314f8fb3518c70e47c2f1d635c5b823d
Carmen Luna <lunacarmen22@gmail.com>
2023-07-13 14:41:29 -0500

    Function to abs and exist

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-module.exports = () => {
-  // ...
+
+const fs = require("fs");
+const path = require("path");
+
+const mdLinks = () => {
+  return new Promise((resolve, reject) => {
+    const relativePath = process.argv[2];
+
+    // DF 1 
+    // Check if the path exists.
+    if (fs.existsSync(relativePath)) {  
+      
+      // DF 2
+      // Check if a path is absolute
+      if (!path.isAbsolute(relativePath)) {
+
+        // DF 3 
+        // Convert the path to an absolute path
+        const absolutePath = path.resolve(relativePath);
+        console.log(absolutePath);
+        resolve(absolutePath);
+
+        // DF 4 
+        // Check if it is a directory
+        if (
+      } else {
+        resolve(relativePath);
+      }
+    } else {
+      reject("the path doesn't exist");
+    }
+  }).catch((error) => {
+    console.error(error); // "Handling the promise rejection
+  });
 };
+
+module.exports = {
+  mdLinks,
+};
+
+mdLinks();
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
 
 
 describe('mdLinks', () => {
@@ -6,5 +6,13 @@ describe('mdLinks', () => {
   it('should...', () => {
     console.log('FIX ME!');
   });
-
+  // it('return promise', () => {
+  //   expect (typeof mdLinks).toBe(Promise);
+  // });
+  it('return false when the path doesn´t exist', () => {
+    return mdLinks.mdLinks("/document/doesntexiste.md").catch((error) => {
+      expect (error).toBe("the path doesn't exist");
+      })
+  });
 });
+
