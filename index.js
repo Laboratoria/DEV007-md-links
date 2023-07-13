bd108844db55f432cc2628ec01ab4067c3994dfa
Analía Klein <analiaklein@gmail.com>
2023-07-13 17:56:49 -0400

    primer funcion

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-module.exports = () => {
-  // ...
-};
+// aqui van todas las funciones
+// importo la libreria path por el require
+const path = require('path');
+
+// la ruta al archivo que se recibe por parametro y procces.argv son los argu que se le pasan al node cuando corre
+const parameterPath = process.argv[1];
+
+// creo la variable donde guardo la ruta absoluta
+let absolutPath = '';
+
+if(path.isAbsolute(parameterPath)) {
+  absolutPath = parameterPath;
+} else {
+  // con el resolve convierte la ruta en absoluta
+  absolutPath = path.resolve(parameterPath);
+}
+
+console.log(absolutPath);
