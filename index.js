ad6ef5bca74fbd076a3718c67c2de658af386d06
Carmen Luna <lunacarmen22@gmail.com>
2023-07-17 17:30:45 -0500

    get all path .md

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
-
+const { ExtractLinks } = require("markdown-link-extractor");
 const fs = require("fs");
 const path = require("path");
 
-const mdLinks = () => {
+const mdLinks = (inputPath = process.argv[2]) => {
   return new Promise((resolve, reject) => {
-    const relativePath = process.argv[2];
-
-    // DF 1 
+    // DF 1
     // Check if the path exists.
-    if (fs.existsSync(relativePath)) {  
-      
+    if (fs.existsSync(inputPath)) {
       // DF 2
       // Check if a path is absolute
-      if (!path.isAbsolute(relativePath)) {
-
-        // DF 3 
+      if (!path.isAbsolute(inputPath)) {
+        // DF 3
         // Convert the path to an absolute path
-        const absolutePath = path.resolve(relativePath);
+        const absolutePath = path.resolve(inputPath);
         console.log(absolutePath);
         resolve(absolutePath);
 
-        // DF 4 
-        // Check if it is a directory
-        if (
+        // DF 4
+        // Check if it is a directory and read it
+        const isDirectory = fs.statSync(absolutePath).isDirectory();
+        if (isDirectory === true) {
+          // aqui exploro el directorio
+          const getMdFiles = (dirPath) => {
+            // DF 5
+            const files = fs.readdirSync(dirPath); // aqui tengo lista de archivos/subdirectorios(S/A)
+            files.forEach((file) => {
+              // recorro esa lista
+              const filePath = path.join(dirPath, file); // aqui defino la ruta de cada archivo
+              const isSubDirectory = fs.statSync(filePath).isDirectory();
+              if (isSubDirectory) {
+                getMdFiles(filePath); // aqui entra mi exploracion de sub directorios (recursividad)
+              } else if (path.extname(filePath) === ".md") {
+                // es un archivo .md?
+                console.log(filePath);
+              }
+            });
+          };
+          getMdFiles(absolutePath);
+        }
+      } else if (path.extname(inputPath) === ".md") {
+        console.log(inputPath);
       } else {
-        resolve(relativePath);
+        reject("The path does not exist .md file");
       }
+      resolve(inputPath); //aqui resuelvo la promesa de mi funcion mdLink
     } else {
-      reject("the path doesn't exist");
+      reject("The path doesn't exist");
     }
   }).catch((error) => {
-    console.error(error); // "Handling the promise rejection
+    console.log(error);
   });
 };
 
@@ -39,4 +57,6 @@ module.exports = {
 };
 
 mdLinks();
+
+
 
