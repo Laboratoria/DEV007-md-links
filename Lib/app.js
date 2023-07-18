b8551b6780689e9f71a13a10c33303cbc5679328
JaviGandarillas <129618844+JaviGandarillas@users.noreply.github.com>
2023-07-18 12:25:08 -0400

    prueba error lens

---
+++ Lib/app.js
@@ -0,0 +1,4 @@
+import saludar from './saludo.js';
+
+// const index = require('./index').saludar;
+saludar('lamomia');
---
+++ Lib/saludo.js
@@ -0,0 +1,10 @@
+// REPL Read, evalue
+
+const saludar = (nombre) => {
+  console.log(`hola, ${nombre}`);
+};
+// module.exports.saludar = saludar;
+
+// saludar('jorge');
+export default saludar;
+// console.log(saludar('freeCodeCamp'), 2);
