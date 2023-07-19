73a6f9cac4fce0140f2e2124aa6cd8dfcadfbe6b
JaviGandarillas <129618844+JaviGandarillas@users.noreply.github.com>
2023-07-19 15:59:21 -0400

    avance refactorización de la función mdLinks

--- Lib/mdLinks.js
+++ Lib/mdLinks.js
@@ -1,27 +1,63 @@
-import { existsSync, statSync, readdirSync, readFileSync } from 'fs';
-import { extname, join } from 'path';
-import http from 'http';
-import https from 'https';
-import chalk from 'chalk';
-import { processFile, processDirectory, processLinks } from './functions.js';
+// app.js
+import { existsSync, statSync } from 'fs';
+import { extname } from 'path';
+import { processFile, processDirectory, processLinks } from './utils.js';
 
 const mdLinks = (path, options) => {
   return new Promise((resolve, reject) => {
     try {
       const mdFiles = [];
       const extractedLinks = [];
-      const validLinks = [];
-      const brokenLinks = [];
 
       if (options.validate) {
         if (existsSync(path)) {
           const stats = statSync(path);
           if (stats.isDirectory()) {
             console.log('Processing directory...');
-            processDirectory(path, mdFiles);
-            console.log('mdFiles length:', mdFiles.length);
+            processDirectory(path, mdFiles, extractedLinks)
+              .then(() => {
+                console.log('mdFiles length:', mdFiles.length);
+                return processLinks(extractedLinks, options);
+              })
+              .then((links) => {
+                resolve(links);
+              })
+              .catch((error) => {
+                reject(error);
+              });
           } else if (stats.isFile() && extname(path) === '.md') {
             mdFiles.push(path);
+            processFile(path, extractedLinks);
+            processLinks(extractedLinks, options)
+              .then((links) => {
+                resolve(links);
+              })
+              .catch((error) => {
+                reject(error);
+              });
+          } else {
+            console.log('The path is not a Markdown file');
+          }
+        } else {
+          console.log('The path does not exist');
+        }
+      } else {
+        if (existsSync(path)) {
+          const stats = statSync(path);
+          if (stats.isDirectory()) {
+            console.log('Processing directory...');
+            processDirectory(path, mdFiles, extractedLinks)
+              .then(() => {
+                console.log('mdFiles length:', mdFiles.length);
+                resolve(extractedLinks);
+              })
+              .catch((error) => {
+                reject(error);
+              });
+          } else if (stats.isFile() && extname(path) === '.md') {
+            mdFiles.push(path);
+            processFile(path, extractedLinks);
+            resolve(extractedLinks);
           } else {
             console.log('The path is not a Markdown file');
           }
@@ -29,32 +65,6 @@ const mdLinks = (path, options) => {
           console.log('The path does not exist');
         }
       }
-
-      mdFiles.forEach((file) => {
-        processFile(file, extractedLinks);
-      });
-
-      processLinks(extractedLinks, path, options, validLinks, brokenLinks)
-        .then((links) => {
-          if (options.stats) {
-            const stats = {
-              Total: links.length,
-              Unique: extractedLinks.length,
-            };
-
-            if (options.validate) {
-              stats.Broken = brokenLinks.length;
-            }
-
-            console.log(chalk.bold('Statistics:'));
-            console.log(stats);
-          }
-
-          resolve(validLinks);
-        })
-        .catch((error) => {
-          reject(error);
-        });
     } catch (error) {
       console.error('An error occurred:', error);
       reject(error);
