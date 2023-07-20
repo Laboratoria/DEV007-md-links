f1fea371465a7e55fa6cdb83600e7151b58361de
yanet <yanettr2024@gmail.com>
2023-07-20 09:46:39 -0500

    Extraer los links del archivo markdown

--- src/library.js
+++ src/library.js
@@ -1,27 +1,66 @@
+#!/usr/bin/env node
+
+import path from "path";
 import { promises as fs } from "fs";
+import axios from "axios";
 import { marked } from "marked";
 import { load } from "cheerio";
 
-//Extraer los links del archivo markdown
-function extractLinksFromFile(absolutePath) {
-  return fs.readFile(absolutePath, "utf8").then((fileContent) => {
+
+// Validar la ruta y convertirla a absoluta
+const validatePath = (filePath) => {
+  if (path.isAbsolute(filePath)) {
+    return filePath;
+  } else {
+    return path.resolve(filePath);
+  }
+};
+
+// Validar si es un archivo Markdown (.md)
+const fileMd = (filePath) => {
+  return path.extname(filePath) === ".md";
+};
+
+// Leer el contenido de un archivo
+const readDoc = (filePath) => {
+  return fs.readFile(filePath, "utf-8");
+};
+
+// Extraer los links del archivo markdown
+function extractLinksFromFile(filePath, validate) {
+  return readDoc(filePath).then((fileContent) => {
     const htmlContent = marked(fileContent);
     const dom = load(htmlContent);
 
-    const links = dom("a").map((_,element) => {
-        const link = dom(element).attr("href");
-        const text = dom(element).text();
-        return {  href: link, text: text ,  file: absolutePath};
-      }).get(); 
+    const links = dom("a").map((_, element) => {
+      const href = dom(element).attr("href");
+      const text = dom(element).text();
+      return { href, text };
+    }).get();
 
-      //Validar links
+    if (validate) {
+      const linkPromises = links.map((link) => {
+        return validateLinks(link, filePath);
+      });
 
-      return links    
+      return Promise.all(linkPromises);
+    } else {
+      return links.map((link) => ({
+        href: link.href,
+        text: link.text,
+        file: filePath,
+      }));
+    }
   });
 }
 
-
 // Se exporta la función mdLinks para que pueda ser utilizada desde otro archivo.
 export {
-  extractLinksFromFile
+  extractLinksFromFile,
 };
+
+
+
+
+
+
