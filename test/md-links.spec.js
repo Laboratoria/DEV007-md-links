34d7f8b83e63f10135764a9890d797b71b33238d
yanet <yanettr2024@gmail.com>
2023-07-24 15:09:07 -0500

    test-3

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -77,6 +77,9 @@ describe('extractLinksFromFile', () => {
   });
 });
 
+
+
+
 describe('extractLinksFromDirectory', () => {
   it('Devuelve una lista de objetos con href, text, file, status y ok cuando options.validate es true', () => {
     const folderPathTest = './test/files';
@@ -146,5 +149,91 @@ describe('extractLinksFromDirectory', () => {
     return extractLinksFromDirectory(folderPathTest, validate).then(links => {
       expect(links).toEqual(expectedLinks);
     });
+  });
+});
+
+describe('countLinks', () => {
+  const links = [
+    {
+      href: 'https://es.wikipedia.org/wiki/Markdown',
+      text: 'Markdown',
+      file: 'test\\files\\prueba2.md',
+      status: 200,
+      ok: 'ok'
+    },
+    {
+      href: 'https://github.com/kenruizinouett-node-fetch-ejemplo/blob/app.js',
+      text: 'Github fetch',
+      file: 'test\\files\\prueba2.md',
+      status: 404,
+      ok: 'fail'
+    },
+    {
+      href: 'https://nodejs.org/es/',
+      text: 'Node.js',
+      file: 'test\\files\\prueba2.md',
+      status: 200,
+      ok: 'ok'
+    },
+    {
+      href: 'https://nodejs.org/es/',
+      text: 'Node.js',
+      file: 'test\\files\\prueba2.md',
+      status: 200,
+      ok: 'ok'
+    }
+  ];
+
+  it('Debe retornar un objeto con links totales, únicos y rotos', () => {
+    const options = {
+      validate: true,
+      stats: true
+    };
+
+    const expectedStats = { total: 4, unique: 3, broken: 1 };
+    expect(countLinks(links, options)).toEqual(expectedStats);
+  });
+});
+
+describe('countLinks', () => {
+  const links = [
+      {
+        href: 'https://es.wikipedia.org/wiki/Markdown',
+        text: 'Markdown',
+        file: 'test\\files\\prueba2.md',
+        status: 200,
+        ok: 'ok'
+      },
+      {
+        href: 'https://github.com/kenruizinouett-node-fetch-ejemplo/blob/app.js',
+        text: 'Github fetch',
+        file: 'test\\files\\prueba2.md',
+        status: 404,
+        ok: 'fail'
+      },
+      {
+        href: 'https://nodejs.org/es/',
+        text: 'Node.js',
+        file: 'test\\files\\prueba2.md',
+        status: 200,
+        ok: 'ok'
+      },
+      {
+        href: 'https://nodejs.org/es/',
+        text: 'Node.js',
+        file: 'test\\files\\prueba2.md',
+        status: 200,
+        ok: 'ok'
+      }
+    ];
+
+  it('Debe retornar un objeto con links totales y únicos', () => {
+    const options = {
+      validate: false, 
+      stats: true
+    };
+
+    const expectedStats = { total: 4, unique: 3 };
+    expect(countLinks(links, options)).toEqual(expectedStats);
   });
 });
