897356746f5c04ea50e42d2abfc955ce58afac73
yanet <yanettr2024@gmail.com>
2023-07-24 16:59:09 -0500

    test 4

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -5,6 +5,16 @@ import {
   mdLinks
 } from '../src/library.js';
 
+it('Debe lanzar un error si la ruta es inválida', () => {
+  const options = {
+    validate: false,
+    stats: false
+  };
+  const invalidPath = './test/files/prueba.md';
+  return expect(mdLinks(options, invalidPath)).rejects.toThrow('La ruta ingresada debe ser un archivo markdown(.md) o un directorio existente.\nRuta: C:\\laboratoria-yanet\\Git\\DEV007-md-links\\test\\files\\prueba.md');
+});
+
+
 describe('extractLinksFromFile', () => {
   it('Devuelve una lista de objetos con href, text, file, status y ok cuando options.validate es true', () => {
     const filePathTest = './test/files/prueba2.md';
@@ -237,3 +247,51 @@ describe('countLinks', () => {
     expect(countLinks(links, options)).toEqual(expectedStats);
   });
 });
+
+describe('mdLinks', () => {
+  it('Debe retornar un array de objetos con los links encontrados en un archivo Markdown', () => {
+    const options = {
+      validate: false,
+      stats: false
+    };
+    const filePathTest = './test/files/prueba2.md';
+
+    return mdLinks(options, filePathTest).then(links => {
+      expect(links.length).toBe(4);
+
+      expect(links[0]).toHaveProperty('href');
+      expect(links[0]).toHaveProperty('text');
+      expect(links[0]).toHaveProperty('file');
+
+      expect(links[0]).not.toHaveProperty('ok');
+      expect(links[0]).not.toHaveProperty('status');
+    });
+  });
+});
+
+describe('mdLinks', () => {
+  it('Debe retornar un array de objetos con los enlaces validados (status y ok) en un archivo Markdown', () => {
+    const options = {
+      validate: true, // Establecer validate en true para habilitar la validación de enlaces
+      stats: false
+    };
+    const filePathTest = './test/files/prueba2.md';
+
+    return mdLinks(options, filePathTest).then(links => {
+      expect(links.length).toBe(4);
+
+      expect(links[0]).toHaveProperty('href');
+      expect(links[0]).toHaveProperty('text');
+      expect(links[0]).toHaveProperty('file');
+
+      // Ahora, los enlaces devueltos también deben tener las propiedades 'ok' y 'status'
+      expect(links[0]).toHaveProperty('ok');
+      expect(links[0]).toHaveProperty('status');
+    });
+  });
+});
+
+
+
+
+
