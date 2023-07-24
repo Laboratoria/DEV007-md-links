1dd4d4be2922624f76435634549fd057a954970a
yanet <yanettr2024@gmail.com>
2023-07-23 23:22:36 -0500

    Test prueba 2

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,7 +1,12 @@
-import { extractLinksFromFile, extractLinksFromDirectory, countLinks, mdLinks } from '../src/library.js';
+import {
+  extractLinksFromFile,
+  extractLinksFromDirectory,
+  countLinks,
+  mdLinks
+} from '../src/library.js';
 
 describe('extractLinksFromFile', () => {
-  it('Devuelve una lista de 4 objetos con href, text, file, status y ok cuando options.validate es true', () => {
+  it('Devuelve una lista de objetos con href, text, file, status y ok cuando options.validate es true', () => {
     const filePathTest = './test/files/prueba2.md';
     const validate = true;
     const expectedLinks = [
@@ -40,9 +45,106 @@ describe('extractLinksFromFile', () => {
     });
   });
 
-   /*
-  it('Devuelve una lista de 4 objetos con href, text, file cuando options.validate es false', () => {
-    
-  }
-  */
+  it('Devuelve una lista de objetos con href, text, file, status y ok cuando options.validate es false', () => {
+    const filePathTest = './test/files/prueba2.md';
+    const validate = false;
+    const expectedLinks = [
+      {
+        href: 'https://es.wikipedia.org/wiki/Markdown',
+        text: 'Markdown',
+        file: './test/files/prueba2.md'
+      },
+      {
+        href: 'https://github.com/kenruizinouett-node-fetch-ejemplo/blob/app.js',
+        text: 'Github fetch',
+        file: './test/files/prueba2.md'
+      },
+      {
+        href: 'https://nodejs.org/es/',
+        text: 'Node.js',
+        file: './test/files/prueba2.md'
+      },
+      {
+        href: 'https://nodejs.org/es/',
+        text: 'Node.js',
+        file: './test/files/prueba2.md'
+      }
+    ];
+
+    return extractLinksFromFile(filePathTest, validate).then(links => {
+      expect(links).toEqual(expectedLinks);
+    });
+  });
+});
+
+describe('extractLinksFromDirectory', () => {
+  it('Devuelve una lista de objetos con href, text, file, status y ok cuando options.validate es true', () => {
+    const folderPathTest = './test/files';
+    const validate = true;
+    const expectedLinks = [
+      {
+        href: 'https://es.wikipedia.org/wiki/Markdown',
+        text: 'Markdown',
+        file: 'test\\files\\prueba2.md',
+        status: 200,
+        ok: 'ok'
+      },
+      {
+        href: 'https://github.com/kenruizinouett-node-fetch-ejemplo/blob/app.js',
+        text: 'Github fetch',
+        file: 'test\\files\\prueba2.md',
+        status: 404,
+        ok: 'fail'
+      },
+      {
+        href: 'https://nodejs.org/es/',
+        text: 'Node.js',
+        file: 'test\\files\\prueba2.md',
+        status: 200,
+        ok: 'ok'
+      },
+      {
+        href: 'https://nodejs.org/es/',
+        text: 'Node.js',
+        file: 'test\\files\\prueba2.md',
+        status: 200,
+        ok: 'ok'
+      }
+    ];
+
+    return extractLinksFromDirectory(folderPathTest, validate).then(links => {
+      expect(links).toEqual(expectedLinks);
+    });
+  });
+
+  it('Devuelve una lista de objetos con href, text, file, status y ok cuando options.validate es false', () => {
+    const folderPathTest = './test/files';
+    const validate = false;
+    const expectedLinks = [
+      {
+        href: 'https://es.wikipedia.org/wiki/Markdown',
+        text: 'Markdown',
+        file: 'test\\files\\prueba2.md'
+      },
+      {
+        href: 'https://github.com/kenruizinouett-node-fetch-ejemplo/blob/app.js',
+        text: 'Github fetch',
+        file: 'test\\files\\prueba2.md'
+      },
+      {
+        href: 'https://nodejs.org/es/',
+        text: 'Node.js',
+        file: 'test\\files\\prueba2.md'
+      },
+      {
+        href: 'https://nodejs.org/es/',
+        text: 'Node.js',
+        file: 'test\\files\\prueba2.md'
+      }
+    ];
+
+    return extractLinksFromDirectory(folderPathTest, validate).then(links => {
+      expect(links).toEqual(expectedLinks);
+    });
+  });
 });
