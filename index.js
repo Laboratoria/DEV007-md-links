34a7761cea278e009e8461e7f4a3448c7f705fb5
Isabella Monsalves <ester.isabella.m@gmail.com>
2023-07-28 18:46:54 -0400

    readMd

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
 //  import node modules
 const fs = require("fs");
 const path = require("path");
-
+const functions = require("./functions.js");
+//  Create function
 const mdLinks = (path, options) => {
   return new Promise((resolve, reject) => {
-    //  identify if route exists
+    //  identify if route exists, method is synchronized, boolean
     if (fs.existsSync(path)) {
-      resolve("La ruta es válida");
-      //  Check type of route, only if route exists
-      const relativePath = process.argv[0];
-      //  route absolute with [0] is C:\Program Files\nodejs\node.exe
-      //  transforme in absoluteroute
-      const absolutePath = path.resolve(relativePath);
-      console.log("ruta relativa:", relativePath);
-      console.log("ruta absoluta:", absolutePath);
-      //  check if is file or directory
-      //  open directory to iterate and filter md files
-      //  md file goes to an array with de md files
-    }
-    //  reject promise if route doesnt exists
-    else {
-      reject("La ruta no existe");
+      // resolve para retornar algo
+      resolve("La ruta sí existe");
+      //  Check type of route(boolean) with .isAbsolute, only if path exists
+      if (!functions.pathIsAbsolute(path)) {
+        //  transform in absolute path if the return is false(relative)
+        userPath = path.resolve(path);
+        console.log(userPath);
+      } else {
+        //  else:keep original path
+        userPath = path;
+      }
+      //  check if it is file or directory
+      var stats = fs.statSync(userPath);
+      console.log(stats.isFile);
+      //  check if it is a file and if it is a file.md
+      if (stats.isFile() && path.extname(userPath) === ".md") {
+        console.log("es un archivo válido" + stats.isFile());
+        //  md file goes to an array with de md files
+        const saveFiles = [];
+        saveFiles.push(userPath);
+        //  read file to check if md file contains Link
+        // este método lee el texto, es asíncrono, devuelve string con el resultado
+        // acá hay que buscar la manera de identificar los links
+        //  método de un vídeo
+        const readText = async (res) => {
+          await fs.readFile(userPath, "utf-8");
+          console.log(res);
+        };
+        // método de Chris
+        fs.readFile(userPath, "utf-8", (res, data) => {
+          console.log(data);
+        });
+        //  save the links in an array
+      } else {
+        //  open directory to iterate and filter md files
+      }
+    } else {
+      //  reject promise if route doesnt exists
+      reject("¡ERROR! La ruta no existe");
     }
   });
 };
-//  export of function mdLinks
+console.log(path.extname("README.md"));
+//  export function mdLinks
 module.exports = {
   mdLinks,
 };
+console.log(mdLinks("./README.md"));
